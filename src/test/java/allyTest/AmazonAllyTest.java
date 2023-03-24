3c336757f7649a6d8f77a7ff8afb7f59d9a7ce48
swapnil <s21dwivedi@gmail.com>
2023-03-24 12:47:58 +0530

    A11y Test code

--- src/test/java/allyTest/AmazonAllyTest.java
+++ src/test/java/allyTest/AmazonAllyTest.java
@@ -1,5 +1,7 @@
 package allyTest;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 
 import org.json.JSONArray;
@@ -8,65 +10,109 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.deque.axe.AXE;
 
+import io.github.sridharbandi.AxeRunner;
+
 public class AmazonAllyTest {
 
 	WebDriver driver;
+
 	private static final URL scriptUrl = AmazonAllyTest.class.getResource("/axe.min.js");
 
 	@BeforeMethod
-	public void setup() {
+	public void setup() throws IOException {
 		driver = new ChromeDriver();
-		driver.get("https://www.amazon.in/");
+		driver.get("https://www.upgrad.com/");
+		AxeRunner axeRunner = new AxeRunner(driver);
+		axeRunner.execute();
+		axeRunner.generateHtmlReport();
+
 	}
 
 	@Test
-	public void amazonAllyTest() {
+	public void amazonAllyTest() throws IOException {
+		driver.navigate().refresh();
+
 		JSONObject responseJson = new AXE.Builder(driver, scriptUrl).analyze();
+
 		JSONArray violations = responseJson.getJSONArray("violations");
+		System.out.println("Violations=>" + violations.length());
+
 		if (violations.length() == 0) {
 			System.out.println("No errors");
+
 		} else {
+
+			System.out.println(violations.length() + " " + "violations found.");
+			// name of the JsonReport ="amazonAllyTest"
 			AXE.writeResults("amazonAllyTest", responseJson);
+			// Test should fail coz we are showing the a11y violations
 			Assert.assertTrue(false, AXE.report(violations));
+
 		}
 
 	}
 
 	@Test
-	public void testAccessabilitySelector() {
-		JSONObject responseJson = new AXE.Builder(driver, scriptUrl).include("title").analyze();
+	public void testAccessabilitySelector() throws IOException {
+		driver.navigate().refresh();
+		JSONObject responseJson = new AXE.Builder(driver, scriptUrl).include("script").analyze();
 		JSONArray violations = responseJson.getJSONArray("violations");
+		System.out.println("Violations=>" + violations.length());
 		if (violations.length() == 0) {
 			System.out.println("No errors");
+
 		} else {
+			System.out.println(violations.length() + " " + "violations found.");
 			AXE.writeResults("testAccessabilitySelector", responseJson);
 			Assert.assertTrue(false, AXE.report(violations));
 		}
 	}
 
 	@Test
-	public void testAccessabilityWithWebElement() {
+	public void testAccessabilityWithTextLinked() {
+		driver.navigate().refresh();
 		JSONObject responseJson = new AXE.Builder(driver, scriptUrl)
 				.analyze(driver.findElement(By.linkText("Customer Service")));
 		JSONArray violations = responseJson.getJSONArray("violations");
 		if (violations.length() == 0) {
 			System.out.println("No errors");
+
 		} else {
-			AXE.writeResults("testAccessabilityWithWebElement", responseJson);
+			System.out.println(violations.length() + " " + "violations found.");
+			AXE.writeResults("testAccessabilityWithTextLinked", responseJson);
 			Assert.assertTrue(false, AXE.report(violations));
+		}
+
+	}
+
+	/**
+	 * Test a WebElement
+	 */
+	@Test
+	public void testAccessibilityWithWebElement() {
+		driver.navigate().refresh();
+
+		JSONObject responseJSON = new AXE.Builder(driver, scriptUrl).analyze(driver.findElement(By.tagName("p")));
+		JSONArray violations = responseJSON.getJSONArray("violations");
+		System.out.println("===" + violations.length());
+		if (violations.length() == 0) {
+			System.out.println("No errors");
+		} else {
+			System.out.println(violations.length() + " " + "violations found.");
+			AXE.writeResults("testAccessibilityWithWebElement", responseJSON);
+			Assert.assertTrue(false, AXE.report(violations));
+
 		}
 	}
 
-	// new AxeBuilder(driver,scriptUrl).analyze(driver);
-
 //	@AfterMethod
 	public void tearDown() {
+
 		driver.quit();
 	}
 }
